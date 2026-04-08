e0be2a02e8496a91b0f030bb16d73f96a433a30a
alexbababu <alexander.janisch@gmail.com>
2026-04-08 21:39:57 +0200

    ; fix

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -409,7 +409,7 @@ int main(int argc, char *argv[]) {
        **/
       // Compute control to apply
       pid_throttle.UpdateError(error_throttle);
-      double throttle = pid_throttle.TotalError()
+      double throttle = pid_throttle.TotalError();
       std::cout << "!!---- throttle: " << throttle << endl;
       throttle = throttle - 0.25*abs(steer_output);
       std::cout << "!!---- steer output: " << steer_output << " gained steer output: " << 0.25*abs(steer_output) << " ----!!" << endl;
